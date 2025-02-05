762bc3688c3f063b7bf698e0984b20327f3a5aea
Varghese Joseph <vj.apps.others@gmail.com>
2025-02-05 23:49:17 +0900

    RAG Langchain assignment12 initial version

---
+++ 04_Answer_Question_RAG_Langchain_LLMs.py
@@ -0,0 +1,6 @@
+# 12. Assignments using RAG and Langchain: Use Lang chain and Retrieval 
+# Augmented Generation, (RAG), and answer a question from the user using 
+# multiple PDF documents connected to the LLM using OpenAI. You can read the 
+# question and refer the multiple PDF documents to answer the question. You 
+# can use the APIs of any other open source LLM too.
+
