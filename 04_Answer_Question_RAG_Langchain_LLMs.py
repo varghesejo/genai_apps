12849666479fa615b8a6f2c63d0c30e42b0b2509
Varghese Joseph <vj.apps.others@gmail.com>
2025-02-06 23:32:05 +0900

    Comments updated

--- 04_Answer_Question_RAG_Langchain_LLMs.py
+++ 04_Answer_Question_RAG_Langchain_LLMs.py
@@ -4,12 +4,6 @@
 # question and refer the multiple PDF documents to answer the question. You 
 # can use the APIs of any other open source LLM too.
 
-# Assignment.12. Assignments using RAG and Langchain: Use Lang chain and 
-# Retrieval Augmented Generation, (RAG), and answer a question from the user 
-# using multiple PDF documents connected to the LLM using OpenAI. You can read
-#  the question and refer the multiple PDF documents to answer the question. 
-# You can use the APIs of any other open source LLM too.
-
 # There is version dependency with python and other dependent libraries, 
 # so using python 3.12.x based environment for installing the libraries
 
